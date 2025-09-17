df6fe9dd81b26975c10439206be75a60d6183cde
nguyen toan <nguyenthitoannn1410@gmail.com>
2025-09-17 11:12:13 +0700

    [Feat] - Add CoreDataService for local persistence

--- GoodFood_App/Services/CoreDataService/CoreDataService.swift
+++ GoodFood_App/Services/CoreDataService/CoreDataService.swift
@@ -3,13 +3,12 @@
 //  GoodFood_App
 //
 //  Created by Guest User on 1/7/25.
-//if let data = entity.weighHistory {
+// if let data = entity.weighHistory {
 //    let history = try? JSONDecoder().decode([WeightRecord].self, from: data)
-//}
+// }
 
 import CoreData
 import Foundation
-
 
 class CoreDataService {
     static let shared = CoreDataService()
@@ -68,17 +67,18 @@ class CoreDataService {
             entity.weight = user.weight ?? 0.0
             entity.targetWeight = user.targetWeight ?? 0.0
             entity.age = Int32(user.age ?? 0)
-            
+
 //            if var weighHistory = user.weighHistory, var weightUser = user.weight {
 //                weighHistory.append(WeightRecord(weight: weightUser) ?? WeightRecord())
             if var weighHistory = user.weighHistory, let weightUser = user.weight {
                 let newRecord = WeightRecord(weight: weightUser)
                 // Chỉ thêm nếu khác bản ghi gần nhất (tránh trùng)
-                    if weighHistory.last?.weight != newRecord.weight ||
-                       abs((weighHistory.last?.date.timeIntervalSince(newRecord.date) ?? 9999)) > 60 {
-                        weighHistory.append(newRecord)
-                    }
-                
+                if weighHistory.last?.weight != newRecord.weight ||
+                    abs((weighHistory.last?.date.timeIntervalSince(newRecord.date) ?? 9999)) > 60
+                {
+                    weighHistory.append(newRecord)
+                }
+
                 do {
                     let encoded = try JSONEncoder().encode(weighHistory)
                     entity.weighHistory = encoded
@@ -114,6 +114,10 @@ class CoreDataService {
         } catch {
             print("Lỗi khi lưu user vào Core Data: \(error.localizedDescription)")
         }
+    }
+
+    func hasUserInCoreData() -> Bool {
+        return CoreDataService.shared.fetchUserModel() != nil
     }
 
     func fetchUser() -> (UserEntity?, [WeightRecord]?) {
@@ -169,7 +173,7 @@ class CoreDataService {
                 print("Lịch sử cân nặng: \(user.weighHistory)")
 
                 // Chuyển về UserModel
-                var weightHistory: [WeightRecord]? = nil
+                var weightHistory: [WeightRecord]?
                 if let historyData = user.weighHistory {
                     weightHistory = try? JSONDecoder().decode([WeightRecord].self, from: historyData)
                 }
@@ -231,8 +235,8 @@ class CoreDataService {
         record.carbs = carbs
         record.protein = protein
         record.fat = fat
-        
-        print("Đã lưu kcalIn vào coredata nghe: \(record.kcalIn) - \(record.carbs)")
+
+        print("Đã lưu KcalIn vào coredata nghe: \(record.kcalIn) - \(record.carbs)")
         saveContext()
     }
 
@@ -245,9 +249,8 @@ class CoreDataService {
             return []
         }
     }
-    
-    func addNutriFood(dish: Dish){
+
+    func addNutriFood(dish: Dish) {
         let context = container.viewContext
-        
     }
 }
